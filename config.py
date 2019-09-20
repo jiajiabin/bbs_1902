a263126a975b03e27d052d5a53bfd53e98f13198
jiajiabin <2592668397@qq.com>
2019-09-20 21:29:23 +0800

    尝试增加富文本编辑器

--- config.py
+++ config.py
@@ -18,3 +18,12 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # 初始定义，每次用户得到session重新赋值
 CMS_USER_ID = 'ASDFSADFDSFSDFSDF'
 Front_USER_ID = 'ASDFSADFDSFSDFSDF'
+
+# 富文本编辑器 ueditor 的配置文件
+UEDITOR_UPLOAD_PATH = os.path.join(os.path.dirname(__file__), 'ufiles')
+# 先注释其中的七牛云配置
+# UEDITOR_UPLOAD_TO_QINIU = True
+# UEDITOR_QINIU_ACCESS_KEY = "p_p2-jutlTI1mlPCSfMEO8DyZnkQaiFrd9IOlvpz"
+# UEDITOR_QINIU_SECRET_KEY = "rN0YQux570vbhL5d8QvShrV-SnjzTdqhlfWstWri"
+# UEDITOR_QINIU_BUCKET_NAME = "bbs1902"
+# UEDITOR_QINIU_DOMAIN = "http://pxwhlyank.bkt.clouddn.com/"
