2a82516bb05c8c6ae4a99fcd20dd08770cc2d182
jiajiabin <2592668397@qq.com>
2019-09-26 20:35:17 +0800

    增加标签模板

--- apps/msg/models.py
+++ apps/msg/models.py
@@ -20,3 +20,16 @@ class ArticleText(db.Model):
     data_text = db.Column(db.Text,nullable=False)
     uid = db.Column(db.Integer, db.ForeignKey("front_user_article.id"))
     articles = db.relationship("Front_User_Article", backref=db.backref("texts", uselist=False))
+
+# 绑定发帖的评论数据，数据库模板
+class Article_Comment(db.Model):
+    # 数据库存储数据
+    __tablename__ = 'article_comment'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # 发帖人name
+    comment_name = db.Column(db.String(50), nullable=False)
+    comment_text = db.Column(db.Text,nullable=False)
+    post_time = db.Column(db.DateTime, default=datetime.now)
+    # 外链连接帖子表，1对多
+    uid = db.Column(db.Integer,db.ForeignKey("front_user_article.id"))
+    articles = db.relationship("Front_User_Article",backref="comment")
